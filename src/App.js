a4f7a60b3d786caf5a8b11b087dcddeca488dac8
Unknown <nebras.z.othman@gmail.com>
2018-10-21 01:59:37 +0200

    WritingCommentsWithSomeStyling

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Paper from '@material-ui/core/Paper';
 class App extends Component {
 
     state = {
-        NowtilusUUID: uuidv1(),
+        NowtilusUUID: uuidv1(), /*Creating Random UUID "there was no specification what kind of uuid" */
         imdbID: "",
         Title: "",
         Plot: "",
@@ -24,7 +24,7 @@ class App extends Component {
         selectedValues: [],
         loadedData: {}
     }
-
+    /*the function responsible for fetching the data and fill or correct the data in the form */
     loadingData = () => {
         if (this.state.Title.length > 0 && this.state.NowtilusUUID.length > 0 && this.state.Released.length > 0) {
             fetch(`http://www.omdbapi.com/?t=${this.state.Title}&i=${this.state.imdbID}&apikey=32671f58`).then(response => {
@@ -48,7 +48,7 @@ class App extends Component {
                         })
                         this.setState({Genre, selectedValues})
 
-                    } else if (property === "Released") {
+                    } else if (property === "Released") { /*I needed to use else if condition because the Date format from the API is different from the date format in the input type */
                         let ReleasedArray = [];
                         let ReleasedData = data[property]
                             .split(" ")
@@ -60,6 +60,7 @@ class App extends Component {
                         const monthNumber = [
                             "01","02","03","04","05","06","07","08","09","10","11","12"
                         ]
+                        /* converting the date format from 15 Feb 2011   to 2011-02-15 that match the input type date format */
                         month.forEach(item => {
                             if (item === ReleasedArray[1]) {
                                 ReleasedArray[1] = monthNumber[month.indexOf(item)]
@@ -80,15 +81,17 @@ class App extends Component {
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         if (this.state.Title !== prevState.Title || this.state.Released !== prevState.Released) {
+            /*calling the loading data function whenever there is a change in the sate */
             this.loadingData()
         }
 
     }
-
+    /* Handeling the change of the inputs */
     handelChange = e => {
         this.loadingData()
         if (e.target) {
             if (Array.isArray(this.state[e.target.name])) {
+                /* converting the String with "," to an Array */
                 const entryArray = e
                     .target
                     .value
@@ -103,9 +106,8 @@ class App extends Component {
             }
         }
     }
-
+    /* this Function to deal with the Multi create select components that I used as an example with the filed Genry just to show it to you because I think it is a nicer UI for multi select options to create the ability */
     onCreateOption = (inputValue) => {
-        console.log(inputValue)
         const Genre = [...this.state.Genre]
         Genre.push(inputValue)
         const selectedValues = [...this.state.selectedValues]
@@ -117,7 +119,7 @@ class App extends Component {
         selectedValues.push(newSelect)
         this.setState({Genre, selectedValues})
     }
-
+    /*Handeling adding multable Ratings options */
     handelClickRatings = e => {
         let currentRatings = [...this.state.Ratings]
         let newRatings = {
@@ -132,6 +134,8 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>Nowtilus Assement</h1>
+                <h4>Entering Movies Form</h4>
+                <p>Using this Form you can fill the required fields and automatically the rest of the fields will be filled from http://www.omdbapi.com API, You can't change the data after it is fetched from the Api so there will don't be conflict. But we could implement he possibility to change the data after the fetch</p>
                 <form >
                 <fieldset>
                 <legend>Enter New Movie:</legend>
@@ -221,7 +225,7 @@ class App extends Component {
                             style={{width: "auto", height: "15px"}}>
                             Add New Rating
                         </Button>
-                        <Paper style={{marginLeft: "10px",padding:"5px"}}>
+                        <Paper style={{margin: "10px",padding:"5px"}}>
                         {this
                             .state
                             .Ratings
