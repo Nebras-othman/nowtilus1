c23f194a51c72da977ceac49748677ae46e8b1e8
Unknown <nebras.z.othman@gmail.com>
2018-10-20 13:42:21 +0200

    adding-main-element-of-the-form

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import React, {Component} from 'react';
+import './App.css';
+import TextField from '@material-ui/core/TextField';
+import FormGroup from '@material-ui/core/FormGroup';
+import CreatableSelect from 'react-select/lib/Creatable';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Nowtilus Assement</h1>
+
+                <TextField
+                    required
+                    label="NowtilusUUID"
+                    margin="normal"
+                    className="inputfield"
+                    value="312"
+                    defaultValue="HI"></TextField>
+
+                <TextField
+                    label="IMDBID"
+                    margin="normal"
+                    className="inputfield"
+                    value="312"
+                    defaultValue="HI"></TextField>
+
+                <TextField
+                    required
+                    label="Title"
+                    margin="normal"
+                    className="inputfield"
+                    value="312"
+                    defaultValue="HI"></TextField>
+
+                <TextField
+                    required
+                    label="Short synopsis "
+                    margin="normal"
+                    className="inputfield"
+                    value="312"
+                    defaultValue="HI"></TextField>
+
+                <TextField
+                    required
+                    label="Release Date"
+                    margin="normal"
+                    className="inputfield"
+                    type="date"></TextField>
+
+                <TextField
+                    required
+                    label="Studio"
+                    margin="normal"
+                    className="inputfield"
+                    value="312"
+                    defaultValue="HI"></TextField>
+                <FormGroup row>
+                    <TextField
+                        required
+                        label="Ratings source"
+                        margin="normal"
+                        className="inputfield"
+                        value="312"
+                        defaultValue="HI"></TextField>
+                    <TextField
+                        required
+                        label="Ratings Value"
+                        margin="normal"
+                        className="inputfield"
+                        value="312"
+                        defaultValue="HI"></TextField>
+                </FormGroup>
+
+                <TextField
+                    required
+                    label="Actors"
+                    margin="normal"
+                    className="inputfield"
+                    value="312"
+                    defaultValue="HI"></TextField>
+
+                <TextField
+                    required
+                    label="Director"
+                    margin="normal"
+                    className="inputfield"
+                    value="312"
+                    defaultValue="HI"></TextField>
+                <TextField
+                    required
+                    label="Writer"
+                    margin="normal"
+                    className="inputfield"
+                    value="312"
+                    defaultValue="HI"></TextField>
+
+            </div>
+        );
+    }
+}
+
+export default App;
